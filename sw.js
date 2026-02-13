12218d261f9f1f16f3844a78c82f241852d57658
mmert925-netizen <mmert925@gmail.com>
2026-02-13 07:53:30 +0300

    Mobil sohbet düzeltmesi

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'omerai-v1';
+const CACHE = 'omerai-v2';
 const ASSETS = ['/', '/index.html', '/style.css', '/script.js', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
